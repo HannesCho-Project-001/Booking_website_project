fec6492d5e59b9aebdef3b339e0263918b00e0f1
HannesCho <hoonsungcho11@gmail.com>
2022-09-02 19:51:10 +0200

    User admin

--- users/models.py
+++ users/models.py
@@ -30,4 +30,3 @@ class User(AbstractUser):
     bio = models.TextField(blank=True)
     birthdate = models.DateField(blank=True, null=True)
     language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
