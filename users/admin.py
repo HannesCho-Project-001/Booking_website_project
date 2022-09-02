1f44eb9a9e8cfc39bec0ca3129ff0ac0b32fd3a5
hoonsung_dev <hoonsung.dev@gmail.com>
2022-09-02 19:45:12 +0200

    User admin

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,26 @@
 from atexit import register
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
 @admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
-    pass
+class CustomUserAdmin(UserAdmin):
+    """Custom User Admin"""
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
